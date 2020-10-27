863f660c56e553dfebc974d7e02f8f7deedd68d2
Rodney <rtm36@zips.uakron.edu>
2020-10-27 13:10:34 -0400

    remove debugging

--- api/getUserYTToken.ts
+++ api/getUserYTToken.ts
@@ -5,10 +5,7 @@ const refreshYTAccessToken = require('./_refreshYTAccessToken');
 
 const getUserYTToken = async (req: NowRequest, res: NowResponse) => {
 
-  let debugList:string[] = ['init'];
   try {
-    debugList.push("inside try. server_token == numbercrunch token" + (req.body.server_token == process.env.NUMBERCRUNCH_TOKEN).toString())
-
     if (req.body.server_token == process.env.NUMBERCRUNCH_TOKEN) {
       if (
         req.body.twitch_username === null ||
@@ -18,23 +15,16 @@ const getUserYTToken = async (req: NowRequest, res: NowResponse) => {
         res.status(401).send('Incorrect twitch_username');
       } else {
         // have a good server token and twitch username
-        debugList.push("about to connect to database");
         const db = await connectToDatabase();
-        debugList.push("connected to database");
         const user_results = await db
           .collection('users')
           .find({ twitch_username: req.body.twitch_username })
           .toArray();
-        debugList.push("user results received" + user_results.toString())
         if (user_results.length !== 0) {
           // return their yt_token
           // eslint-disable-next-line
-          debugList.push("user results > 0" + user_results.length.toString())
           const refresh_token = user_results[0]['youtube_credentials']['refresh_token'];
-          debugList.push("awaiting refresh YT Access Token" + refresh_token.toString())
-          debugList.push("environment clientID: " + process.env.YOUTUBE_CLIENT_ID + " secret: " + process.env.YOUTUBE_CLIENT_SECRET)
           const refreshedYoutubeCredentials = await refreshYTAccessToken(refresh_token);
-          debugList.push("received refreshedYoutubeCredentials" + refreshedYoutubeCredentials.toString())
 
           // filter
         const filter = { twitch_username: req.body.twitch_username };
@@ -50,17 +40,13 @@ const getUserYTToken = async (req: NowRequest, res: NowResponse) => {
           token_type: refreshedYoutubeCredentials.token_type,
         }
 
-
-        debugList.push("received refreshedYoutubeCredentials" + JSON.stringify(youtube_credentials))
         const updatedoc = {
           $set: {
             youtube_credentials: youtube_credentials
           },
         };
-        debugList.push("about to update users table")
         db.collection('users').updateOne(filter, updatedoc, options);
-        debugList.push("updated users table")
-        res.status(200).send(debugList);
+        res.status(200).send(JSON.stringify(youtube_credentials));
         } else {
           res.status(401).send('twitch_username does not exist');
         }
@@ -69,7 +55,7 @@ const getUserYTToken = async (req: NowRequest, res: NowResponse) => {
       res.status(401).send('Incorrect server token');
     }
   } catch(err) {
-    res.status(400).send(debugList);
+    res.status(400).send('Request Failed');
   }
 };
 
