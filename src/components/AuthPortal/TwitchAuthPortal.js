75d4897e9955523a05b5e6a6f0e18e0507d5b350
Rodney <rtm36@zips.uakron.edu>
2020-10-27 13:30:03 -0400

    hardcode twitch redirect_uri

--- src/components/AuthPortal/TwitchAuthPortal.js
+++ src/components/AuthPortal/TwitchAuthPortal.js
@@ -8,7 +8,7 @@ export default function TwitchAuthPortal() {
     // const redirectURI = (window.location.hostname === "localhost") ? "http://localhost:8000/TwitchAuth" : 'https://dev.clipclock.stream/TwitchAuth';
     const redirectURI = 'https://dev.clipclock.stream/TwitchAuth';
     window.open(
-      `https://id.twitch.tv/oauth2/authorize?client_id=${clientID}&redirect_uri=${redirectURI}&response_type=token&scope=user_read`,
+      `https://id.twitch.tv/oauth2/authorize?client_id=${clientID}&redirect_uri=https://dev.clipclock.stream/TwitchAuth&response_type=token&scope=user_read`,
       'Login to Twitch',
       'width=600px, height=400px, left=200px, top=200px',
     );
