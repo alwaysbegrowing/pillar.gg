7a53a224eaef51c430e513e512ce836ac1ed4285
Steven Gates <steven@deephire.com>
2020-11-05 17:39:42 -0800

    Update defaultSettings.ts

--- config/defaultSettings.ts
+++ config/defaultSettings.ts
@@ -12,7 +12,7 @@ export default {
   menu: {
     locale: true,
   },
-  title: 'ClipClock',
+  title: 'Pillar',
   pwa: false,
   logo: 'https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg',
   iconfontUrl: '',
