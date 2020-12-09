5a6dddf1d4f3f65cd8d3dec90858c67ecee53a1e
Rodney <rtm36@zips.uakron.edu>
2020-12-09 00:56:33 -0500

    modify syntax to potentially fix vercel error

--- api/user/login.ts
+++ api/user/login.ts
@@ -18,6 +18,7 @@ const login = async (req: NowRequest, res: NowResponse) => {
 
     // get access token from auth token
     const twitch_access_info = await getUserTwitchCredentials(auth_token);
+    console.log('twitch credentials received');
     // get credentials
     const twitchUserData = await getTwitchUserData(twitch_access_info.access_token);
     // set twitch credentials to be updated
@@ -26,33 +27,29 @@ const login = async (req: NowRequest, res: NowResponse) => {
     let twitch_id;
     let twitch_profile_picture;
     let twitch_credentials;
-    if(twitchUserData !== null) {
+    if (twitchUserData !== null) {
       username = twitchUserData.name;
       email = twitchUserData.email;
       twitch_id = twitchUserData.twitch_id;
       twitch_profile_picture = twitchUserData.twitch_profile_picture;
       twitch_credentials = {
-          access_token: twitch_access_info.access_token,
-          refresh_token: twitch_access_info.refresh_token,
-          expires_in: twitch_access_info.expires_in,
-          scope: twitch_access_info.scope,
-          token_type: twitch_access_info.token_type,
-      }
-    }
-    else{
+        access_token: twitch_access_info.access_token,
+        refresh_token: twitch_access_info.refresh_token,
+        expires_in: twitch_access_info.expires_in,
+        scope: twitch_access_info.scope,
+        token_type: twitch_access_info.token_type,
+      };
+    } else {
       res.status(401).send('Error retrieving twitch credentials');
     }
 
     // connect to database and check for user by twitch id
     const db = await connectToDatabase();
 
-    const result = await db
-      .collection('users')
-      .findOne({ twitch_id: twitch_id });
+    const result = await db.collection('users').findOne({ twitch_id: twitch_id });
 
     // if user exists in database
     if (result) {
-
       // filter by twitch id
       const filter = { twitch_id: result.twitch_id };
 
@@ -71,6 +68,7 @@ const login = async (req: NowRequest, res: NowResponse) => {
       db.collection('users').updateOne(filter, updatedoc, options);
 
       const isYoutubeLinked = result.youtube_credentials ? true : false;
+
       const userInfo = {
         avatar: twitch_profile_picture,
         name: username,
@@ -78,12 +76,10 @@ const login = async (req: NowRequest, res: NowResponse) => {
         user_id: result._id,
         access: result.plan,
         youtubeLinked: isYoutubeLinked,
-        twitch_access_token: twitch_credentials?.access_token
-      }
-      res.status(200).json(userInfo)
-    }
-    else {
-
+        twitch_access_token: twitch_credentials ? twitch_credentials.access_token : null,
+      };
+      res.status(200).json(userInfo);
+    } else {
       // get registration timestamp
       const timestamp = Date.now();
 
@@ -91,21 +87,27 @@ const login = async (req: NowRequest, res: NowResponse) => {
       // When Payment is integrated, the stripe connection would go here
       const plan = 'none';
 
-
       const stripeCustomerID = await postNewStripeCustomer(email, username);
       // stripe customer could not be created
-      if(stripeCustomerID === null) {
-        res.status(502).send("Request failed because a stripe account wasn't created.")
+      if (stripeCustomerID === null) {
+        res.status(502).send("Request failed because a stripe account wasn't created.");
       }
       // stripe account has been created and all above information is valid
       else {
         // add user to database
-        await addUser(email, twitch_id, username, twitch_profile_picture, stripeCustomerID, twitch_credentials, timestamp, plan);
+        await addUser(
+          email,
+          twitch_id,
+          username,
+          twitch_profile_picture,
+          stripeCustomerID,
+          twitch_credentials,
+          timestamp,
+          plan,
+        );
 
         // receive userid generated from mongo
-        const user_result = await db
-          .collection('users')
-          .findOne({ twitch_id });
+        const user_result = await db.collection('users').findOne({ twitch_id });
 
         // send client success status and user_id for client side authentication
 
@@ -120,11 +122,10 @@ const login = async (req: NowRequest, res: NowResponse) => {
           user_id: user_result._id,
           access: user_result.plan,
           youtubeLinked: isYoutubeLinked,
-          twitch_access_token: user_result.twitch_credentials.access_token
-        }
-        res.status(200).json(userInfo)
+          twitch_access_token: user_result.twitch_credentials.access_token,
+        };
+        res.status(200).json(userInfo);
       }
-
     }
   } catch (err) {
     res.status(400).json({ error: err });
