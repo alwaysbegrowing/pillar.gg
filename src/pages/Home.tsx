d3bc7c7b85a2faaf0773cb5a2c483624747017bf
Rodney <rtm36@zips.uakron.edu>
2021-02-25 15:58:00 -0500

    add basic dropdown implementation

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,74 +1,66 @@
 import React, {useState, useEffect} from 'react';
 import { PageContainer } from '@ant-design/pro-layout';
-import { Card } from 'antd';
-import {useModel } from 'umi';
+import { Card, Menu, Dropdown } from 'antd';
+// import {useModel } from 'umi';
+import { DownOutlined } from '@ant-design/icons';
+
 // import styles from './Home.less';
-import YoutubeAuthPortal from '../components/AuthPortal/YoutubeAuthPortal';
-import LinkYoutubeButton from '../components/LinkYoutubeButton/LinkYoutubeButton';
 
 const Home = () => {
 
-  const [openYoutubeAuthPortal, toggleYoutubeAuthPortal] = useState(false);
-  const [shouldShowLinkButton, toggleLinkButton] = useState(true);
-  const { initialState, setInitialState } = useModel('@@initialState');
+
+  const [titleList, setTitleList ] = useState<string[]>([]);
+  // const { initialState, setInitialState } = useModel('@@initialState');
 
   useEffect(() => {
+    fetch('/api/twitch/getVodList', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        // user_id: initialState!.currentUser!.user_id
+        // mock using ludwig's user_id to get good data
+        user_id: '5fce7ed8b1514498a4e5e679'
+      }),
+    })
+    .then(res =>
+      res.json()
+    )
+    .then(json => {
+      var titles:Array<string> = [];
+      json.videos.forEach((video: {title:string}) => {
+        titles.push(video.title)
+      });
+      setTitleList(titles);
+    })
+    .catch(err => {
+      console.log(err.message)
+    });
+  },[]);
 
-    getYoutubeSyncStatus();
-    window.onmessage = (event: any) => {
-      if (event.data.success) {
-        fetch('/api/youtube/postUserYTToken', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({
-            user_id: initialState!.currentUser!.user_id,
-            code: event.data.code,
-          }),
-        })
-        .then((res) => {
-          if(res.status === 200) {
-            let currentUser = initialState?.currentUser;
-            if(currentUser?.youtubeLinked === false) {
-              currentUser.youtubeLinked = true;
-              // non-null assertation operator is appropriate because
-              // this page can't be reached without initial state being set
-              setInitialState({...initialState!, currentUser});
-            }
-          }
-        });
-      }
-    };
-  })
-
-  useEffect(() => {
-    toggleYoutubeAuthPortal(false);
-  }, [openYoutubeAuthPortal])
-
-  function getYoutubeSyncStatus() {
-    // button will not show if current user has youtube account linked
-    if(initialState?.currentUser?.youtubeLinked) {
-      toggleLinkButton(false);
-    }
-    else {
-      toggleLinkButton(true);
-    }
-  }
-
-  function toggleAuthPortal() {
-    toggleYoutubeAuthPortal(true);
+  function setMenu(){
+    let submenu = titleList.map((title) =>
+        <Menu.Item>
+          {title}
+        </Menu.Item>
+    );
+    let menu = <Menu>{submenu}</Menu>
+    return(menu);
   }
 
     return (
       <PageContainer>
         <Card>
-          <h1>Welcome to ClipClock</h1>
-            { shouldShowLinkButton && <LinkYoutubeButton onClick={() => toggleAuthPortal()} /> }
-          {openYoutubeAuthPortal && <YoutubeAuthPortal />}
+          <Dropdown overlay={setMenu()}>
+            <a className="ant-dropdown-link" onClick={e => e.preventDefault()}>
+              Select a VOD <DownOutlined />
+            </a>
+          </Dropdown>
         </Card>
       </PageContainer>
     );
   }
 
 export default Home;
+
