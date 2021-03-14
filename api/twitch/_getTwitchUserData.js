cb9cd7fca362ff9db99675130ac045a67369e316
russell <russell@deephire.com>
2021-03-14 07:14:30 -0500

    ALWAYS use async/await over promises.

--- api/twitch/_getTwitchUserData.js
+++ api/twitch/_getTwitchUserData.js
@@ -1,35 +1,26 @@
 const fetch = require('node-fetch');
 
-// This function takes in the access token and returns
-// the username and email associated with the account in an object
 const getTwitchUserData = async (access_token) => {
   const url = `https://api.twitch.tv/kraken/user`;
   const client_id = process.env.TWITCH_CLIENT_ID;
-  
-  const data = await fetch(url, {
-    mode: "no-cors",
-    method: "GET",
-    headers: {
-      'Accept': 'application/vnd.twitchtv.v5+json',
-      'Authorization': `OAuth ${access_token}`,
-      'Client-ID': client_id,
-    }
-  })
-  .then((res => {
-    if(!res.ok) {
-      return(null);
-    }
-    return(res);
-  }))
-  .then((resp => resp.json()))
-  .then((json => {
-    // eslint-disable-next-line
-    return({email: json.email, name: json.name, twitch_id: json._id, twitch_profile_picture: json.logo});
-  }))
-  .catch(() => {
-    return null;
-  });
-  return data;
+
+  try {
+    const data = await fetch(url, {
+      mode: 'no-cors',
+      method: 'GET',
+      headers: {
+        Accept: 'application/vnd.twitchtv.v5+json',
+        Authorization: `OAuth ${access_token}`,
+        'Client-ID': client_id,
+      },
+    });
+    if (!data.ok) return null;
+    const resp = await data.json();
+    const { email, name, _id: twitch_id, logo: twitch_profile_picture } = resp;
+    return { email, name, twitch_id, twitch_profile_picture };
+  } catch (e) {
+    return e;
+  }
 };
 
 module.exports = getTwitchUserData;
