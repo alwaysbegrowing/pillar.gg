881fd89a255013651ff35f8f9103994f575793d6
Rodney <rtm36@zips.uakron.edu>
2021-04-22 18:16:22 -0400

    add user as Contact in Hubspot CRM on initial login

---
+++ api/hubspot/_addContact.ts
@@ -0,0 +1,46 @@
+const hubspot = require('@hubspot/api-client');
+
+interface TwitchUserData {
+  id: string;
+  login: string;
+  display_name: string;
+  type: string;
+  broadcaster_type: string;
+  description: string;
+  profile_image_url: string;
+  offline_image_url: string;
+  view_count: number;
+  email: string;
+  created_at: string;
+}
+
+export default async (twitchUserData: TwitchUserData) => {
+  try {
+    const contactObj = {
+      properties: {
+        twitch_id: twitchUserData.id,
+        twitch_login_name: twitchUserData.login,
+        twitch_display_name: twitchUserData.display_name,
+        twitch_account_type: twitchUserData.type, // might need to test how sending "" works
+        twitch_broadcaster_type: twitchUserData.broadcaster_type,
+        twitch_description: twitchUserData.description,
+        twitch_profile_image_url: twitchUserData.profile_image_url,
+        twitch_offline_image_url: twitchUserData.offline_image_url,
+        twitch_view_count: twitchUserData.view_count,
+        email: twitchUserData.email,
+      },
+    };
+    const hubspotClient = new hubspot.Client({ apiKey: process.env.HUBSPOT_API_KEY });
+    const createContactResponse = await hubspotClient.crm.contacts.basicApi.create(contactObj);
+    return { hubspot_contact_id: createContactResponse.body.id };
+  } catch (e: any) {
+    if (e.statusCode === 409) {
+      const existingHubspotClientID = e.response.body.message.replace(
+        'Contact already exists. Existing ID: ',
+        '',
+      );
+      return existingHubspotClientID;
+    }
+    return { hubspot_contact_id: null };
+  }
+};
--- api/user/login.ts
+++ api/user/login.ts
@@ -1,5 +1,6 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 import getTwitchUserData from '../twitch/_getTwitchUserData';
+import addHubspotContact from '../hubspot/_addContact';
 
 const connectToDatabase = require('../_connectToDatabase');
 const getUserTwitchCredentials = require('../twitch/_getUserTwitchCredentials');
@@ -15,12 +16,17 @@ const login = async (req: VercelRequest, res: VercelResponse) => {
 
     const twitchUserData = await getTwitchUserData(credentials.access_token);
 
+    const hubspotID = await addHubspotContact(twitchUserData);
+
     const db = await connectToDatabase();
 
-    const filter = { twitch_id: twitchUserData.id };
+    const filter = {
+      twitch_id: twitchUserData.id,
+      hubspot_contact_id: hubspotID.hubspot_contact_id,
+    };
     const options = { upsert: true };
     const updatedoc = {
-      $set: twitchUserData,
+      $set: Object.assign(twitchUserData, hubspotID),
     };
 
     db.collection('users').updateOne(filter, updatedoc, options);
