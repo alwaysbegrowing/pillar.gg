8c0b39ca518bad5da414cfa5c296e262d347f434
Matt <info@mgates.me>
2021-05-07 11:54:26 -0500

    Fix #58

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -1,11 +1,5 @@
-import {
-  CaretRightOutlined,
-  NotificationOutlined,
-  PauseOutlined,
-  SoundOutlined,
-} from '@ant-design/icons';
-import { Row, Slider, Space, Spin } from 'antd';
-import React, { useCallback, useEffect, useState } from 'react';
+import { Spin } from 'antd';
+import React, { useEffect, useState } from 'react';
 import ReactPlayer from 'react-player/twitch';
 
 const styles = {
@@ -33,54 +27,16 @@ interface VideoProps {
   playing: boolean;
   setPlaying: React.Dispatch<React.SetStateAction<boolean>>;
 }
-const Video = ({
-  url,
-  duration,
-  progress,
-  onProgress,
-  videoRef,
-  controlKeys,
-  playing,
-  setPlaying,
-}: VideoProps) => {
-  const [muted, setMuted] = useState(false);
+const Video = ({ url, onProgress, videoRef, playing }: VideoProps) => {
   const [isLoading, setIsLoading] = useState(false);
-  const [hover, setHover] = useState(true);
-
-  const handleEnter = useCallback(
-    (event) => {
-      const { code } = event;
-      if (code === 'Space' && controlKeys) {
-        event.preventDefault();
-        setPlaying((isPlaying) => !isPlaying);
-      }
-    },
-    [setPlaying, controlKeys],
-  );
-
-  useEffect(() => {
-    window.addEventListener('keydown', handleEnter);
-    return () => window.removeEventListener('keydown', handleEnter);
-  }, [controlKeys, handleEnter]);
 
   useEffect(() => {
     setIsLoading(false);
   }, [url]);
 
-  const togglePlaying = () => {
-    setPlaying((isPlaying) => !isPlaying);
-  };
-  const toggleMuted = () => {
-    setMuted((isMuted) => !isMuted);
-  };
-
   return (
     <Spin spinning={!isLoading}>
-      <div
-        onMouseEnter={() => setHover(true)}
-        onMouseLeave={() => setHover(true)}
-        style={styles.playerWrapper}
-      >
+      <div style={styles.playerWrapper}>
         <ReactPlayer
           onReady={() => setIsLoading(true)}
           style={styles.reactPlayer}
@@ -88,56 +44,10 @@ const Video = ({
           height="100%"
           width="100%"
           playing={playing}
-          muted={muted}
+          controls
           onProgress={onProgress}
           url={url}
         />
-        <div
-          onClick={togglePlaying}
-          style={{
-            position: 'absolute',
-            top: 0,
-            height: 'calc(100% - 50px)',
-            width: '100%',
-          }}
-        />
-        <div
-          style={{
-            width: '100%',
-            bottom: 0,
-            position: 'absolute',
-            display: hover || !playing ? 'block' : 'none',
-            backgroundColor: 'rgba(0,0,0,.7)',
-          }}
-        >
-          <Slider
-            style={{
-              marginLeft: 8,
-              marginRight: 8,
-              marginBottom: 0,
-              paddingTop: 4,
-              paddingBottom: 4,
-            }}
-            max={duration}
-            value={progress}
-          />
-
-          <Row justify="space-between" style={{ marginLeft: 16, marginRight: 16, marginBottom: 8 }}>
-            <Space size="middle">
-              {playing ? (
-                <PauseOutlined style={styles.icon} onClick={togglePlaying} />
-              ) : (
-                <CaretRightOutlined style={styles.icon} onClick={togglePlaying} />
-              )}
-              {muted ? (
-                <NotificationOutlined style={styles.icon} onClick={toggleMuted} />
-              ) : (
-                <SoundOutlined style={styles.icon} onClick={toggleMuted} />
-              )}
-              <div style={{ fontSize: 12, color: 'white' }}>{`${progress} / ${duration}`}</div>
-            </Space>
-          </Row>
-        </div>
       </div>
     </Spin>
   );
