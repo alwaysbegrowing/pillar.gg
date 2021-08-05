a29a641b7ca374c5b496149ddf37b856a347fe98
chand1012 <chandler@chand1012.dev>
2021-08-05 18:46:00 -0400

    Limit number of CCCs to find

--- src/pages/Vods.tsx
+++ src/pages/Vods.tsx
@@ -1,10 +1,14 @@
 import { PageContainer } from '@ant-design/pro-layout';
 import { List } from 'antd';
-import React from 'react';
+import React, { useState } from 'react';
 import { useVideos } from '../services/hooks/api';
 import VodCard from '../components/VodCard';
 import SelectUser from '../components/SelectUser';
 import { fetcher } from '@/services/fetcher';
+
+// number of vods to go back
+// and check for CCC
+const CCC_VOD_CHECK = 5;
 
 const shouldRenderExtra = () => {
   const { NODE_ENV } = process.env;
@@ -16,17 +20,19 @@ const shouldRenderExtra = () => {
 };
 
 const Videos = () => {
+  const [ranCCCFinder, setCCCFinder] = useState(false);
   const { data: videos, isLoading, isError } = useVideos();
 
   if (isLoading) return 'loading...';
   if (isError) return 'error';
 
-  if (!isLoading && !isError) {
-    videos.forEach((video: { id: any }) => {
+  if (!isLoading && !isError && !ranCCCFinder) {
+    videos.slice(0, CCC_VOD_CHECK).forEach((video: { id: any }) => {
       if (video?.id) {
         fetcher(`/api/ccc/${video.id}`);
       }
     });
+    setCCCFinder(true);
   }
 
   return (
