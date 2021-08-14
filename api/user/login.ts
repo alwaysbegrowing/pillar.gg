28477a83c06a354ffa210914238a73960a309447
Russell Ratcliffe <russell@deephire.com>
2021-08-14 13:51:19 -0500

    Revert "Add SNS trigger on new user sign up"

--- api/user/login.ts
+++ api/user/login.ts
@@ -1,7 +1,6 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 import getTwitchUserData from '../twitch/_getTwitchUserData';
 import addHubspotContact from '../hubspot/_addContact';
-import { PublishCommand, SNSClient } from '@aws-sdk/client-sns';
 
 const connectToDatabase = require('../_connectToDatabase');
 const getUserTwitchCredentials = require('../twitch/_getUserTwitchCredentials');
@@ -24,43 +23,12 @@ const login = async (req: VercelRequest, res: VercelResponse) => {
     const filter = {
       twitch_id: twitchUserData.id,
     };
-
+    const options = { upsert: true };
     const updatedoc = {
       $set: { ...twitchUserData, ...hubspotID },
     };
 
-    const resp = await db.collection('users').updateOne(filter, updatedoc, {});
-
-    // if the item in the database doesn't exist, insert it
-    if (resp.result.nModified === 0) {
-      const newUserData = {
-        ...twitchUserData,
-        ...hubspotID,
-      };
-
-      db.collection('users').insertOne(newUserData);
-
-      const snsCredentials = {
-        accessKeyId: process.env.SIGNUPEVENT_AWS_ACCESS_KEY_ID || '',
-        secretAccessKey: process.env.SIGNUPEVENT_AWS_SECRET_ACCESS_KEY || '',
-      };
-
-      const sns = new SNSClient({ region: 'us-east-1', credentials: snsCredentials });
-
-      const command = new PublishCommand({
-        Message: 'Pillar has a new user!',
-        MessageAttributes: {
-          TwitchId: {
-            DataType: 'String',
-            StringValue: twitchUserData.id,
-          },
-        },
-        TopicArn: process.env.SNS_TOPIC_ARN,
-      });
-
-      await sns.send(command);
-    }
-
+    db.collection('users').updateOne(filter, updatedoc, options);
     res.status(200).json(credentials);
   } catch (err) {
     res.status(400).json({ error: err.message });
