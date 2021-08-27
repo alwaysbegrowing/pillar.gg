2f1395b3295ba5dc6411bc3311753e41968101fd
Sam Swindell <swindesr@users.noreply.github.com>
2021-08-27 00:10:20 -0400

    Remove slider src files

--- src/components/Slider/src/Slider/index.ts
+++
@@ -1,8 +0,0 @@
-import {
-  SliderProps as _SliderProps,
-  SliderState as _SliderState,
-} from './types';
-export { Slider } from './Slider';
-
-export type SliderProps = _SliderProps;
-export type SliderState = _SliderState;
--- src/components/Slider/src/utils.ts
+++
@@ -1,5 +0,0 @@
-export function callAll<T>(...fns: (((e: T) => void) | undefined)[]) {
-  return (e: T) => {
-    return fns.forEach(fn => fn && fn(e));
-  };
-}
