7675dc6723bb91d955f63e03e5928001537476b3
chand1012 <chandler@chand1012.dev>
2021-08-31 18:08:18 -0400

    Add additional events to custom events

--- api/hubspot/_customEvents.ts
+++ api/hubspot/_customEvents.ts
@@ -1,3 +1,6 @@
 export const LOGIN_EVENT = 'pe9257754_login';
 export const SIGNUP_EVENT = 'pe9257754_signupevent';
 export const MAJOR_EVENT = 'pe9257754_major_event';
+export const SAVED_ADJUSTED_CLIP_EVENT = 'pe9257754_saved_adjusted_clip';
+export const EXPORT_CLIP_EVENT = 'pe9257754_export_clip';
+export const CLICKED_CLIP_EVENT = 'pe9257754_clicked_clip';
