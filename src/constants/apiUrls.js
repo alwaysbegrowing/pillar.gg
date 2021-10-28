70b52067a112880d969f562552506e50e1990e4f
chand1012 <chandler@chand1012.dev>
2021-10-28 16:03:35 -0400

    Switch clip export to Vercel API

--- src/constants/apiUrls.js
+++ src/constants/apiUrls.js
@@ -1,7 +1,4 @@
 export const MOBILE_EXPORT_URL =
   'https://lfh9xm104e.execute-api.us-east-1.amazonaws.com/prod/export';
 
-export const CLIP_EXPORT_URL = 'https://lfh9xm104e.execute-api.us-east-1.amazonaws.com/prod/clips';
-
-export const CLIP_EXPORT_URL_DEV =
-  'https://jbme5m4076.execute-api.us-east-1.amazonaws.com/prod/clips';
+export const CLIP_EXPORT_URL = '/api/export/clips';
