6faea06009009fa419a48dc4174c85e6211de96a
Teddy Byron <ted@tedbyron.com>
2021-10-27 21:31:33 -0400

    always redirect to correct url on twitch auth fix #230

--- api/twitch/_getUserTwitchCredentials.js
+++ api/twitch/_getUserTwitchCredentials.js
@@ -1,14 +1,16 @@
 const fetch = require('node-fetch');
+const { NODE_ENV } = process.env;
 
 const getUserTwitchCredentials = async (code) => {
   const url =
-    `https://id.twitch.tv/oauth2/token?` +
-    `code=${code}&` +
-    `client_id=${process.env.TWITCH_CLIENT_ID}&` +
-    `client_secret=${process.env.TWITCH_CLIENT_SECRET}&` +
-    // `redirect_uri=http://localhost:8000/TwitchAuth&`+
-    `redirect_uri=https://app.pillar.gg/TwitchAuth&` +
-    `grant_type=authorization_code`;
+    `https://id.twitch.tv/oauth2/token` +
+    `?code=${code}` +
+    `&client_id=${process.env.TWITCH_CLIENT_ID}` +
+    `&client_secret=${process.env.TWITCH_CLIENT_SECRET}` +
+    (NODE_ENV === 'production'
+      ? `&redirect_uri=https://app.pillar.gg/TwitchAuth`
+      : `&redirect_uri=http://localhost:8000/TwitchAuth`) +
+    `&grant_type=authorization_code`;
 
   const data = await fetch(url, {
     method: 'POST',
