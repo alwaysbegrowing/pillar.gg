f477b8e3c7ef02a068ae4ed03bc1a2d83a9d16f7
Teddy Byron <ted@tedbyron.com>
2021-10-29 00:08:42 -0400

    add fullstory to app.tsx

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import type { BasicLayoutProps } from '@ant-design/pro-layout';
+import * as FullStory from '@fullstory/browser';
 
 import RightContent from '@/components/RightContent';
 import Footer from '@/components/Footer';
 import defaultSettings from '../config/defaultSettings';
 import { ContextWrapper } from './ContextWrapper';
+
+const fullStoryOpts: FullStory.SnippetOptions = { orgId: '167CBS' };
+
+if (process.env.NODE_ENV !== 'production') {
+  // fullStoryOpts.debug = true;
+  // fullStoryOpts.devMode = true;
+}
+
+// `FS.getCurrentSessionURL()` in console to view session
+FullStory.init(fullStoryOpts);
 
 export const layout = (): BasicLayoutProps & {
   childrenRender?: (dom: JSX.Element) => React.ReactNode;
