11e31e19bb013241845eb482ff2007b7441f533b
Teddy Byron <ted@tedbyron.com>
2021-11-02 11:39:44 -0400

    add fullstory identification

--- src/pages/TwitchAuth.tsx
+++ src/pages/TwitchAuth.tsx
@@ -1,26 +1,42 @@
 import React, { useEffect } from 'react';
 import { Spin } from 'antd';
-// import { useUser } from '@/services/hooks/api';
-// import * as FullStory from '@fullstory/browser';
+import * as FullStory from '@fullstory/browser';
+import { twitchClientId } from '@/services/fetcher';
 
 // TOOO REMOVE this page and just show the /home page. In the /home page we can show the app skeleton giving the illusion that they aren't waiting
 export default () => {
   useEffect(() => {
-    const code = new URLSearchParams(window.location.search).get('code') ?? '';
+    const login = async () => {
+      // get auth code
+      const code = new URLSearchParams(window.location.search).get('code') || '';
 
-    fetch(`/api/user/login?code=${code}`)
-      .then((response) => response.json())
-      .then((result) => localStorage.setItem('access_token', result.access_token))
-      .then(() => {
-        // get twitch user data
-        // { data, error } = useUser();
+      // get access token
+      const loginResponse = await fetch(`/api/user/login?code=${code}`);
+      const loginResult = await loginResponse.json();
+      const accessToken = loginResult.access_token;
+      localStorage.setItem('access_token', accessToken);
 
-        // if (error) { throw error }
-        // else {}
-        // identify FullStory user
-
-        window.close();
+      // get user info
+      const userResponse = await fetch('https://api.twitch.tv/helix/users', {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+          'Client-ID': twitchClientId,
+        },
       });
+      const { data } = await userResponse.json();
+      const userData = data?.[0];
+
+      if (userData) {
+        FullStory.identify(userData.id, {
+          display_name: userData.display_name,
+          email: userData.email,
+        });
+      }
+
+      window.close();
+    };
+
+    login();
   }, []);
 
   return (
