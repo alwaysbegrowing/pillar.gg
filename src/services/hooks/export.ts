80da30ca4b2dcef8206966a8fd9b83f3e7c4919f
chand1012 <chandler@chand1012.dev>
2021-11-04 16:04:28 -0400

    Delete functions no longer needed

--- src/services/hooks/export.ts
+++ src/services/hooks/export.ts
@@ -1,11 +1,6 @@
 import useSWR from 'swr';
 import { fetcher } from '../fetcher';
 
-const useExport = (id: string) => {
-  const { data, error } = useSWR(`/api/export/get/video/${id}`, fetcher, { refreshInterval: 1000 });
-  return { data, isLoading: !error && !data, isError: error };
-};
-
 const useExports = (page: number, perPage: number = 10, userId: string | number | null = null) => {
   const userIdString = userId ? `&userId=${userId}` : '';
   const { data, error } = useSWR(
@@ -16,4 +11,4 @@ const useExports = (page: number, perPage: number = 10, userId: string | number 
   return { data, isLoading: !error && !data, isError: error };
 };
 
-export { useExport, useExports };
+export { useExports };
