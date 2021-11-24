747f59f4e660b9661e7262fd0ed4825d858b7758
chand1012 <chandler@chand1012.dev>
2021-11-24 18:05:23 -0500

    Add client refresh code

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,3 +1,24 @@
 // refresh the twitch authentication if there
-// was a failed request.
-export const refresh = async () => {};
+// was a failed request. Returns true if successful.
+export const refresh = async () => {
+  // get the refresh token from local storage
+  const refresh_token = localStorage.getItem('refresh_token');
+  if (!refresh_token) {
+    return false;
+  }
+
+  const resp = await fetch(`/api/user/refresh?token=${refresh_token}`);
+
+  if (!resp.ok) {
+    return false;
+  }
+
+  const data = await resp.json();
+
+  const { access_token, refresh_token: new_refresh_token } = data;
+
+  localStorage.setItem('access_token', access_token);
+  localStorage.setItem('refresh_token', new_refresh_token);
+
+  return true;
+};
