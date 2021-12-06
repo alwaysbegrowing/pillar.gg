f40c84f6109b8d6194c59ae6326c52617fa66c6f
Chandler Lofland <chandler@chand1012.dev>
2021-12-05 19:16:21 -0500

    Update retry function

--- src/services/hooks/retry.js
+++ src/services/hooks/retry.js
@@ -1,21 +1,18 @@
 import { login, refreshToken } from '../auth';
 
 const swrErrorRetry = (error, key, config, revalidate, { retryCount }) => {
-  // Never retry on 404.
-  if (error.status === 404) return;
-
-  // Never retry on network errors.
-  if (error.isNetworkError) return;
-
-  // Only retry once.
-  if (retryCount >= 1) {
+  // Only try to log them in via the window
+  // only once
+  if (retryCount === 1) {
     login();
     return;
   }
 
   // attempt to refresh the token
   refreshToken().finally(() => {
-    revalidate({ retryCount });
+    setTimeout(() => {
+      revalidate({ retryCount });
+    }, 1000 * retryCount);
   });
 };
 
