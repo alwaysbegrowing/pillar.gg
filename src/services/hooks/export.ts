082c9a0898abee5743bdf8ba72e16c4de977b1d6
chand1012 <chandler@chand1012.dev>
2021-12-07 18:37:08 -0500

    Add optional query parameters to useExports

--- src/services/hooks/export.ts
+++ src/services/hooks/export.ts
@@ -1,10 +1,29 @@
 import useSWR from 'swr';
 import { fetcher } from '../fetcher';
 
-const useExports = (page: number, perPage: number = 10, userId: string | number | null = null) => {
+export interface ExportsOptions {
+  startDate?: number;
+  endDate?: number;
+  dateSort?: number;
+  platform?: string;
+  platformSort?: number;
+}
+
+const useExports = (
+  page: number,
+  perPage: number = 10,
+  userId: string | number | null = null,
+  options: ExportsOptions = {},
+) => {
+  const { startDate, endDate, dateSort, platform, platformSort } = options;
   const userIdString = userId ? `&userId=${userId}` : '';
+  const startDateString = startDate ? `&startDate=${startDate}` : '';
+  const endDateString = endDate ? `&endDate=${endDate}` : '';
+  const dateSortString = dateSort ? `&dateSort=${dateSort}` : '';
+  const platformString = platform ? `&platform=${platform}` : '';
+  const platformSortString = platformSort ? `&platformSort=${platformSort}` : '';
   const { data, error } = useSWR(
-    `/api/export/get/all?page=${page}&perPage=${perPage}${userIdString}`,
+    `/api/export/get/all?page=${page}&perPage=${perPage}${userIdString}${startDateString}${endDateString}${dateSortString}${platformString}${platformSortString}`,
     fetcher,
     { refreshInterval: 1000 },
   );
