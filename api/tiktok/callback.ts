0b46403cddfe395088be4a4e915313042ff20be7
chand1012 <chandler@chand1012.dev>
2021-12-21 14:26:52 -0500

    Add initial callback endpoint code

---
+++ api/tiktok/callback.ts
@@ -0,0 +1,6 @@
+// import fetch from 'node-fetch';
+import type { VercelRequest, VercelResponse } from '@vercel/node';
+
+const callback = (req: VercelRequest, res: VercelResponse) => {};
+
+export default callback;
