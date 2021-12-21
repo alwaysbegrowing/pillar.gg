931ca94f76b8168b4645882ae15a3625f4baed5e
chand1012 <chandler@chand1012.dev>
2021-12-21 14:22:58 -0500

    Add initial oauth redirect endpoint

---
+++ api/tiktok/oauth.ts
@@ -0,0 +1,29 @@
+import type { VercelRequest, VercelResponse } from '@vercel/node';
+
+// ripped right out of their docs
+// https://developers.tiktok.com/doc/login-kit-web
+
+const SCOPE = 'user.info.basic,share.sound.create,video.upload';
+const { TIKTOK_CLIENT_KEY, NODE_ENV } = process.env;
+
+const REDIRECT_URI =
+  NODE_ENV === 'development'
+    ? 'http://localhost:8000/api/tiktok/callback'
+    : 'https://app.pillar.gg/api/tiktok/callback';
+
+const oauth = async (req: VercelRequest, res: VercelResponse) => {
+  const csrfState = Math.random().toString(36).substring(7);
+  // res.cookie('csrfState', csrfState, { maxAge: 60000 });
+
+  let url = 'https://open-api.tiktok.com/platform/oauth/connect/';
+
+  url += `?client_key=${TIKTOK_CLIENT_KEY}`;
+  url += '&scope=' + SCOPE;
+  url += '&response_type=code';
+  url += '&redirect_uri=' + REDIRECT_URI;
+  url += '&state=' + csrfState;
+
+  res.redirect(url);
+};
+
+export default oauth;
